4b517e25da50de2dff71c8ae0782a09def3e15d7
DimitryGonzales <125162224+DimitryGonzales@users.noreply.github.com>
2026-09-19 15:26:01 -0300

    refactor: replace menu's do-while loop and if statements with a switch-case

--- src/Main.java
+++ src/Main.java
@@ -33,117 +33,125 @@ public class Main {
         boolean menuRepetindo = false;
 
         do {
-            do {
-                if (!menuRepetindo) System.out.println("Menu:");
-                else System.out.println("\nMenu:");
+            if (!menuRepetindo) System.out.println("Menu:");
+            else System.out.println("\nMenu:");
 
-                System.out.println("\t1 - Comprar\n" +
-                        "\t2 - Ver fatura\n" +
-                        "\t3 - Excluir item\n" +
-                        "\t4 - Alterar item\n" +
-                        "\t5 - Finalizar");
-                System.out.print("\t> ");
-                menuOpcao = sc.nextInt();
-
-                if (menuOpcao < 1 || menuOpcao > 5) System.out.println("\nOpção inválida!\n");
-            } while (menuOpcao < 1 || menuOpcao > 5);
+            System.out.println("\t1 - Comprar\n" +
+                    "\t2 - Ver fatura\n" +
+                    "\t3 - Excluir item\n" +
+                    "\t4 - Alterar item\n" +
+                    "\t5 - Finalizar");
+            System.out.print("\t> ");
+            menuOpcao = sc.nextInt();
 
             menuRepetindo = true;
 
-            if (menuOpcao == 1) {
-                System.out.println("\n1 - Comprar:\n");
-
-                boolean produtoExiste = false;
-
-                do {
-                    for (Produto produto : produtos) {
-                        System.out.printf("%s:\n\tCódigo = %s\n\tPreço = R$%.2f\n",
-                                produto.getNome(),
-                                produto.getCodigo(),
-                                produto.getPreco());
-                    }
-
-                    System.out.print("\nCódigo e quantidade(0 para cancelar): ");
-                    String produtoCodigo = sc.next();
-                    int produtoQuantidade = sc.nextInt();
-
-                    if (produtoCodigo.equals("0") || produtoQuantidade <= 0) break;
-
-                    for (Produto produto : produtos) {
-                        if (produto.getCodigo().equals(produtoCodigo)) {
-                            produtoExiste = true;
-
-                            fatura.adicionarItem(new Item(produto, produtoQuantidade));
-
-                            break;
-                        }
-                    }
-
-                    if (!produtoExiste) System.out.println("\nCódigo de produto inválido!\n");
-                } while (!produtoExiste);
-            }
-
-            if (menuOpcao == 2) {
-                System.out.println("\n2 - Ver fatura:\n");
-
-                if (!fatura.getItens().isEmpty()) {
-                    System.out.printf("%s\n", fatura.exibirItens());
-
-                    System.out.printf("\n\tValor final: R$%.2f\n", fatura.getValor());
-                } else System.out.println("A fatura está vazia");
-            }
-
             String itemCodigo;
 
-            if (menuOpcao == 3) {
-                System.out.println("\n3 - Excluir item:\n");
+            switch(menuOpcao) {
+                case 1:
+                    System.out.println("\n1 - Comprar:\n");
 
-                if (!fatura.getItens().isEmpty()) {
-                    itemCodigo = receberItemCodigo(fatura, sc);
+                    boolean produtoExiste = false;
 
-                    if (!itemCodigo.equals("0")) {
-                        for (int i = 0; i < fatura.getItens().size(); i++) {
-                            if (fatura.getItens().get(i).getProduto().getCodigo().equals(itemCodigo)) {
-                                fatura.removerItem(itemCodigo);
+                    while (!produtoExiste) {
+                        for (Produto produto : produtos) {
+                            System.out.printf("%s:\n\tCódigo = %s\n\tPreço = R$%.2f\n",
+                                    produto.getNome(),
+                                    produto.getCodigo(),
+                                    produto.getPreco());
+                        }
+
+                        System.out.print("\nCódigo e quantidade(0 para cancelar): ");
+                        String produtoCodigo = sc.next();
+                        int produtoQuantidade = sc.nextInt();
+
+                        if (produtoCodigo.equals("0") || produtoQuantidade <= 0) break;
+
+                        for (Produto produto : produtos) {
+                            if (produto.getCodigo().equals(produtoCodigo)) {
+                                produtoExiste = true;
+
+                                fatura.adicionarItem(new Item(produto, produtoQuantidade));
 
                                 break;
                             }
                         }
+
+                        if (!produtoExiste) System.out.println("\nCódigo de produto inválido!\n");
                     }
 
-                } else System.out.println("A fatura está vazia");
-            }
+                    break;
 
-            if (menuOpcao == 4) {
-                System.out.println("\n4 - Alterar item:\n");
+                case 2:
+                    System.out.println("\n2 - Ver fatura:\n");
 
-                if (!fatura.getItens().isEmpty()) {
-                    itemCodigo = receberItemCodigo(fatura, sc);
+                    if (!fatura.getItens().isEmpty()) {
+                        System.out.printf("%s\n", fatura.exibirItens());
 
-                    if (!itemCodigo.equals("0")) {
-                        int itemQuantidade;
+                        System.out.printf("\n\tValor final: R$%.2f\n", fatura.getValor());
+                    } else System.out.println("A fatura está vazia");
 
-                        do {
-                            System.out.print("\nQuantidade(0 para cancelar): ");
-                            itemQuantidade = sc.nextInt();
+                    break;
 
-                            if (itemQuantidade < 0) System.out.println("\nA quantidade precisa ser um valor positivo");
-                        } while (itemQuantidade < 0);
+                case 3:
+                    System.out.println("\n3 - Excluir item:\n");
 
-                        if (itemQuantidade > 0) {
+                    if (!fatura.getItens().isEmpty()) {
+                        itemCodigo = receberItemCodigo(fatura, sc);
+
+                        if (!itemCodigo.equals("0")) {
                             for (int i = 0; i < fatura.getItens().size(); i++) {
-                                Item item = fatura.getItens().get(i);
-
-                                if (item.getProduto().getCodigo().equals(itemCodigo)) {
-                                    item.alterarQuantidade(itemQuantidade);
+                                if (fatura.getItens().get(i).getProduto().getCodigo().equals(itemCodigo)) {
+                                    fatura.removerItem(itemCodigo);
 
                                     break;
                                 }
                             }
                         }
-                    }
 
-                } else System.out.println("A fatura está vazia");
+                    } else System.out.println("A fatura está vazia");
+
+                    break;
+
+                case 4:
+                    System.out.println("\n4 - Alterar item:\n");
+
+                    if (!fatura.getItens().isEmpty()) {
+                        itemCodigo = receberItemCodigo(fatura, sc);
+
+                        if (!itemCodigo.equals("0")) {
+                            int itemQuantidade;
+
+                            do {
+                                System.out.print("\nQuantidade(0 para cancelar): ");
+                                itemQuantidade = sc.nextInt();
+
+                                if (itemQuantidade < 0) System.out.println("\nA quantidade precisa ser um valor positivo");
+                            } while (itemQuantidade < 0);
+
+                            if (itemQuantidade > 0) {
+                                for (int i = 0; i < fatura.getItens().size(); i++) {
+                                    Item item = fatura.getItens().get(i);
+
+                                    if (item.getProduto().getCodigo().equals(itemCodigo)) {
+                                        item.alterarQuantidade(itemQuantidade);
+
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                    } else System.out.println("A fatura está vazia");
+
+                    break;
+
+                case 5:
+                    break;
+
+                default:
+                    System.out.println("\nOpção inválida!\n");
             }
         } while (menuOpcao != 5);
     }
